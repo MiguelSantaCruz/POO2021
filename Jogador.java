a873158197b87ae1a7cddd65d96185fa71a730d3
Miguel <a93194@alunos.uminho.pt>
2021-04-13 23:28:42 +0100

    Classe jogador concluida

--- Jogador.java
+++ Jogador.java
@@ -1,9 +1,6 @@
-
 /**
- * Escreva a descrição da classe Jogador aqui.
- * 
- * @author (seu nome) 
- * @version (número de versão ou data)
+ * Classe jogador
+ * @version 1
  */
 public class Jogador
 {
@@ -16,7 +13,7 @@ public class Jogador
     private int capacidade_de_passe;
 
     /**
-     * COnstrutor para objetos da classe Jogador
+     * Construtor vazio para objetos da classe Jogador
      */
     public Jogador()
     {
@@ -29,6 +26,9 @@ public class Jogador
         this.capacidade_de_passe = 0;
     }
     
+    /**
+     * Construtor para objetos da classe Jogador
+     */
     public Jogador(int velocidade, int resistencia, int destreza, int impulsao, int jogo_de_cabeça, int remate, int capacidade_de_passe){
         this.velocidade = velocidade;
         this.resistencia = resistencia;
@@ -37,8 +37,170 @@ public class Jogador
         this.jogo_de_cabeça = jogo_de_cabeça;
         this.remate = remate;
         this.capacidade_de_passe = capacidade_de_passe;
-    
     }
+    
+    /**
+     * Construtor de cópia para objetos da classe Jogador
+     */
+    public Jogador(Jogador j){
+        this.velocidade = j.getVelocidade();
+        this.resistencia = j.getResistencia();
+        this.destreza = j.getDestreza();
+        this.impulsao = j.getImpulsao();
+        this.jogo_de_cabeça = j.getJogoDeCabeca();
+        this.remate = j.getRemate();
+        this.capacidade_de_passe = j.getCapacidadeDePasse();
+    }
+
+    /**
+     * Retorna a velocidade do Jogador
+     */
+    int getVelocidade(){
+        return this.velocidade;
+    }
+
+    /**
+     * Retorna a resistencia do Jogador
+     */
+    int getResistencia(){
+        return this.resistencia;
+    }
+
+    /**
+     * Retorna a destreza do Jogador
+     */
+    int getDestreza(){
+        return this.destreza;
+    }
+
+    /**
+     * Retorna a impulsao do Jogador
+     */
+    int getImpulsao(){
+        return this.impulsao;
+    }
+
+    /**
+     * Retorna o jogo de cabeça do Jogador
+     */
+    int getJogoDeCabeca(){
+        return this.jogo_de_cabeça;
+    }
+
+    /**
+     * Retorna o remate do Jogador
+     */
+    int getRemate(){
+        return this.remate;
+    }
+
+    /**
+     * Retorna a capacidade de passe do Jogador
+     */
+    int getCapacidadeDePasse(){
+        return this.capacidade_de_passe;
+    }
+
+    /**
+     * Atribui velocidade ao Jogador
+     */
+    void setVelocidade(int velocidade){
+        this.velocidade = velocidade;
+    }
+
+    /**
+     * Atribui resistencia ao Jogador
+     */
+    void setResistencia(int resistencia){
+        this.resistencia = resistencia;
+    }
+
+    /**
+     * Atribui destreza ao Jogador
+     */
+    void setDestreza(int destreza){
+        this.destreza = destreza;
+    }
+
+    /**
+     * Atribui impulsao ao Jogador
+     */
+    void setImpulsao(int impulsao){
+        this.impulsao = impulsao;
+    }
+
+    /**
+     * Atribui jogo de cabeça ao Jogador
+     */
+    void setJogoDeCabeca(int jogo_de_cabeça){
+        this.jogo_de_cabeça = jogo_de_cabeça;
+    }
+
+    /**
+     * Atribui remate ao Jogador
+     */
+    void setRemate(int remate){
+        this.remate = remate;
+    }
+
+    /**
+     * Atribui capacidade de passe ao Jogador
+     */
+    void setCapacidadeDePasse(int capacidade_de_passe){
+        this.capacidade_de_passe = capacidade_de_passe;
+    }
+
+    /**
+     * Compara um objeto com Jogador e diz se são iguais
+     */
+    public boolean equals(Object o){
+        if(o.getClass() != this.getClass() || o == null) return false;
+        Jogador j = (Jogador) o;
+        return j.getVelocidade() == this.getVelocidade() && j.getResistencia() == this.getResistencia()
+                && j.getDestreza() == this.getDestreza() && j.getImpulsao() == this.getImpulsao()
+                 &&j.getJogoDeCabeca() == this.getJogoDeCabeca() && j.getRemate() == this.getRemate() 
+                  && j.getCapacidadeDePasse() == this.getCapacidadeDePasse();
+    }
+
+    /**
+     * Clona o objeto Jogador
+     */
+    public Jogador clone(){
+        Jogador j = new Jogador();
+        j.velocidade = this.getVelocidade();
+        j.resistencia = this.getResistencia();
+        j.destreza = this.getDestreza();
+        j.impulsao = this.getImpulsao();
+        j.jogo_de_cabeça = this.getJogoDeCabeca();
+        j.remate = this.getRemate();
+        j.capacidade_de_passe = this.getCapacidadeDePasse();
+        return j;
+    }
+
+    /**
+     * Converte para string o objeto Jogador
+     */
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Velocidade: ");
+        sb.append(getVelocidade());
+        sb.append(" Resistencia: ");
+        sb.append(getResistencia());
+        sb.append(" Destreza: ");
+        sb.append(getDestreza());
+        sb.append(" Impulsão: ");
+        sb.append(getImpulsao());
+        sb.append(" Jogo de cabeça: ");
+        sb.append(getJogoDeCabeca());
+        sb.append(" Remate: ");
+        sb.append(getRemate());
+        sb.append(" Capacidade de passe: ");
+        sb.append(getCapacidadeDePasse());
+        return sb.toString();
+    }
+    
+    
+    
     
         
 
--- Main.java
+++ Main.java
@@ -1,14 +1,23 @@
-
-/**
- * Escreva a descrição da classe Main aqui.
- * 
- * @author (seu nome) 
- * @version (número de versão ou data)
- */
 public class Main
 {
     public static void main(String[] args) {
+        
+        //Teste para Jogador --------------------------------------------------------------------
+        Jogador j0 = new Jogador();
+        Jogador j1 = new Jogador(6,5,3,3,7,8,5);
+        Jogador j2 = new Jogador(j1);
 
+        System.out.println("Jogador 0: " + j0.toString());
+        System.out.println("Jogador 1: " + j1.toString());
+        System.out.println("Jogador 2:" + j2.toString());
+        Jogador j3 = new Jogador();
+        System.out.println("Jogador 3 (Vazio):" + j3.toString());
+        j3 = j1.clone();
+        System.out.println("Jogador 3 (Clonado de Jogador 1):" + j3.toString());
+        System.out.println("Jogador 1 == Jogador 2?" + j1.equals(j2));
+        System.out.println("Jogador 0 == Jogador 2?" + j0.equals(j2));
+        System.out.println("Jogador 1 == Jogador 3 (Clonado de Jogador 1)?" + j1.equals(j3));
+        //---------------------------------------------------------------------------------------
     }
 
 }
